d7a2c5355a8d2beee735988e34f7722b2dfe618c
Jan Seriš <jan.seris@seznam.cz>
2023-03-07 18:44:13 +0100

    refactored objects

--- TheatricalPlayersRefactoringKata/Invoice.cs
+++ TheatricalPlayersRefactoringKata/Invoice.cs
@@ -4,17 +4,12 @@ namespace TheatricalPlayersRefactoringKata
 {
     public class Invoice
     {
-        private string _customer;
-        private List<Performance> _performances;
-
-        public string Customer { get => _customer; set => _customer = value; }
-        public List<Performance> Performances { get => _performances; set => _performances = value; }
-
-        public Invoice(string customer, List<Performance> performance)
+        public string Customer { get; set; }
+        public List<Performance> Performances { get; set; }
+        public Invoice(string customer, List<Performance> performances)
         {
-            this._customer = customer;
-            this._performances = performance;
+            Customer = customer;
+            Performances = performances;
         }
-
     }
 }
